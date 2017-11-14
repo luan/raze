4f0a99fe207f2c0a679eed9f8e26da4611581a70
Luan Santos <cfcluan@gmail.com>
2017-11-14 11:16:07 -0800

    Use pretty box drawing characters

--- src/main.rs
+++ src/main.rs
@@ -54,18 +54,13 @@ fn generate_maze(width: i32, height: i32) -> Vec<Vec<char>> {
         }
     }
 
+    let directions = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
     while tile_sets.len() > 1 {
         let a = rand::thread_rng().gen_range(0, max_point);
         let side = rand::thread_rng().gen_range(0, 3);
 
-        let (diff_x, diff_y) = match side {
-            0 => (-1, 0),
-            1 => (1, 0),
-            2 => (0, -1),
-            3 => (0, 1),
-            _ => panic!("unreachable"),
-        };
-
+        let (diff_x, diff_y) = directions[side];
         let (x_a, y_a) = (a % width, a / width);
         let (x_b, y_b) = (x_a + diff_x, y_a + diff_y);
         if x_b < 0 || x_b >= width || y_b < 0 || y_b >= height {
@@ -93,6 +88,55 @@ fn generate_maze(width: i32, height: i32) -> Vec<Vec<char>> {
             tile_sets.remove(b_index);
         }
     }
+
+    let two: i32 = 2;
+    for y in 0..tiles.len() {
+        for x in 0..tiles[y].len() {
+            if tiles[y][x] != ' ' {
+                let mut walls = 0;
+                for (i, &(xdiff, ydiff)) in directions.iter().enumerate() {
+                    let (oy, ox) = (y as i32 + ydiff, x as i32 + xdiff);
+                    if oy < 0 || oy >= 2 * height + 1 || ox < 0 || ox >= 2 * width + 1 {
+                        continue;
+                    }
+                    if tiles[oy as usize][ox as usize] != ' ' {
+                        walls += two.pow(i as u32);
+                    }
+                }
+
+                tiles[y][x] = match walls {
+                    1 => '╸',
+                    2 => '╺',
+                    3 => '━',
+                    4 => '╹',
+                    5 => '┛',
+                    6 => '┗',
+                    7 => '┻',
+                    8 => '╻',
+                    9 => '┓',
+                    10 => '┏',
+                    11 => '┳',
+                    12 => '┃',
+                    13 => '┫',
+                    14 => '┣',
+                    15 => '╋',
+                    _ => '#',
+                }
+            }
+        }
+    }
+
+    let goal = rand::thread_rng().gen_range(0, max_point);
+    let (goal_x, goal_y) = (goal % width, goal / width);
+    let goal_x = goal_x * 2 + 1;
+    let goal_y = goal_y * 2 + 1;
+    tiles[goal_y as usize][goal_x as usize] = '⚑';
+
+    let start = rand::thread_rng().gen_range(0, max_point);
+    let (start_x, start_y) = (start % width, start / width);
+    let start_x = start_x * 2 + 1;
+    let start_y = start_y * 2 + 1;
+    tiles[start_y as usize][start_x as usize] = '⚉';
 
     tiles
 }
